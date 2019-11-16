a98bf5c927559afacf4b4039123504afc2458086
Otto Hyytiälä <otto.hyytiala@gmail.com>
2019-11-16 20:43:38 +0200

    query by name

--- services/transactionsService.js
+++ services/transactionsService.js
@@ -3,16 +3,33 @@ const Transaction = require('../models/Transaction')
 module.exports.getTransactionsMonth = async (months) => {
   const endDate = new Date()
   endDate.setMonth(endDate.getMonth() - months)
-  const transactions = await Transaction.find({
-    date: {
-      '$gte': endDate,
-      '$lte': Date.now()
-    }
-  }).sort([['date', -1]])
-  return transactions.map(Transaction.format)
+  try {
+    const transactions = await Transaction.find({
+      date: {
+        '$gte': endDate,
+        '$lte': Date.now()
+      }
+    }).sort([['date', -1]])
+    return transactions.map(Transaction.format)
+  } catch (error) {
+    return error
+  }
+}
+
+module.exports.getTransactionsByName = async (name) => {
+  try {
+    const transactions = await Transaction.find({ name: { $regex: name, $options: 'i' } })
+    return transactions.map(Transaction.format)
+  } catch (error) {
+    return error
+  }
 }
 
 module.exports.getAll = async () => {
-  const transactions = await Transaction.find({})
-  return transactions.map(Transaction.format)
+  try {
+    const transactions = await Transaction.find({})
+    return transactions.map(Transaction.format)
+  } catch (error) {
+    return error
+  }
 }
